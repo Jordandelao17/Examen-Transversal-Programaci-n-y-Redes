6e349582ee2f228b22178f8596df2d0d7781d69c
Jordandelao17 <167580306+Jordandelao17@users.noreply.github.com>
2024-06-29 16:21:55 -0400

    archivos
    Archivos Examen Transversal DRY7122

---
+++ listado_integrantes.py
@@ -0,0 +1,16 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # listado_integrantes.py
+... 
+... def main():
+...     integrantes = [
+...         "Jordan Delao Opazo",
+...         # Aquí puedes agregar más nombres si es necesario
+...     ]
+... 
+...     print("Integrantes del grupo:")
+...     for integrante in integrantes:
+...         print("-", integrante)
+... 
+... if __name__ == "__main__":
+...     main()
